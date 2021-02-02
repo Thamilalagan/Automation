e1463c145aa6de5d7b9627b195240639493fa08b
alaga <alaga@LAPTOP-9SAO9B6V.Home>
2021-02-02 21:39:09 +0000

    String.indexOf(String) error

--- DataDrivenFramework/src/test/java/com/w2a/listeners/CustomListeners.java
+++ DataDrivenFramework/src/test/java/com/w2a/listeners/CustomListeners.java
@@ -3,7 +3,6 @@ package com.w2a.listeners;
 import java.io.IOException;
 
 import org.testng.ITestContext;
-//import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 import org.testng.Reporter;
@@ -28,18 +27,44 @@ public class CustomListeners extends TestBase implements ITestListener {
 		test.log(LogStatus.FAIL, result.getName().toUpperCase()+"Failed with exception"+result.getThrowable());
 		test.log(LogStatus.FAIL, test.addScreenCapture(TestUtil.screenshotName));
 
-		rep.endTest(test);
-		rep.flush();
+		
 		Reporter.log("Click to see Screenshot");
 		Reporter.log("<a target = \"_blank\" href ="+TestUtil.screenshotName+"> Screenshot </a>");
 		Reporter.log("<br>");
 		Reporter.log("<a target = \"_blank\" href ="+TestUtil.screenshotName+"> <img src="+TestUtil.screenshotName+" height=100 width=100> </img> </a>");
+		
+		rep.endTest(test);
+		rep.flush();
 
 	}
 
-	public void onStart(ITestContext context) {
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
 		
-		test = rep.startTest(context.getName().toUpperCase());
+	}
+
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestFailedWithTimeout(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestStart(ITestResult result) {
+		test = rep.startTest(result.getName().toUpperCase());
 	}
 
 	public void onTestSuccess(ITestResult result) {
--- DataDrivenFramework/src/test/java/com/w2a/testcases/TestIsElementPresent.java
+++ DataDrivenFramework/src/test/java/com/w2a/testcases/TestIsElementPresent.java
@@ -9,18 +9,37 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestIsElementPresent {
+	
+	public static WebDriver driver;
+	
+	public static boolean isElementPresent(By by) {
+		
+		/*try {
+		driver.findElement(By.xpath(locator));
+		return true;
+		}catch(Throwable t) {
+			return false;
+		}*/
+		
+		int size = driver.findElements(by).size();
+		if(size==0) {
+			return false;
+		}else {
+			return true;
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 		driver.get("https:www.wikipedia.org");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
-		System.out.println(driver.findElement(By.xpath("//*[@id=\'js-link-box-en\']/strong")).isDisplayed());
-		
+		//System.out.println(driver.findElement(By.xpath("//*[@id=\'js-link-box-en\']/strong")).isDisplayed());
+		System.out.println(isElementPresent(By.id("searchLanguage")));
 
 
 	}
