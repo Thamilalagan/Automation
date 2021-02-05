9c9198814e0a5d066570841b5f9a1c253e135e9c
alaga <alaga@LAPTOP-9SAO9B6V.Home>
2021-02-05 15:12:56 +0000

    Actions

---
+++ DataDrivenFramework/src/test/java/com/w2a/testcases/TestDragAndDrop.java
@@ -0,0 +1,32 @@
+package com.w2a.testcases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestDragAndDrop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://jqueryui.com/resources/demos/droppable/default.html");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		WebElement drag = driver.findElement(By.xpath("//*[@id=\"draggable\"]"));
+		WebElement drop = driver.findElement(By.xpath("//*[@id=\"droppable\"]"));
+		
+		Actions action = new Actions(driver);
+		action.dragAndDrop(drag, drop).perform();
+
+	}
+
+}
---
+++ DataDrivenFramework/src/test/java/com/w2a/testcases/TestKeyboardEvents.java
@@ -0,0 +1,39 @@
+package com.w2a.testcases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestKeyboardEvents {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://gmail.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.findElement(By.id("identifierId")).sendKeys("alagan.tamil@gmail.com");
+		driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]")).click();
+		
+		
+		
+		Actions action = new Actions(driver);
+		action.keyDown( Keys.CONTROL ).sendKeys( "a" ).keyUp( Keys.CONTROL ).build().perform();
+		action.keyDown( Keys.CONTROL ).sendKeys( "c" ).keyUp( Keys.CONTROL ).build().perform();
+		 
+		driver.findElement(By.id("identifierId")).click();
+		action.keyDown( Keys.CONTROL ).sendKeys( "v" ).keyUp( Keys.CONTROL ).build().perform();
+		 
+		
+	}
+
+}
